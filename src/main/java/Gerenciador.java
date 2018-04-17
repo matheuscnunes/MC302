ad020c434e5d67928e664bd48afc13a83a5a0d65
gibatalha <giovanna.batalha03@gmail.com>
2018-04-17 13:07:43 -0300

    Implementando métodos

--- src/main/java/Gerenciador.java
+++ src/main/java/Gerenciador.java
@@ -1,24 +1,23 @@
 package main.java;
 
 import entity.Disciplina;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Gerenciador {
-    private List<Professor> professores;
-    private List<Aluno> alunos;
-    private List<Monitor> monitores;
-    private List<Disciplina> disciplinas;
-    private List<Turma> turmas;
-    private Usuario usuarioAtual;
+    private static List<Professor> professores = new ArrayList<Professor>();
+    private static List<Aluno> alunos = new ArrayList<Aluno>();
+    private static List<Monitor> monitores = new ArrayList<Monitor>();
+    private static List<Disciplina> disciplinas = new ArrayList<Disciplina>();
+    private static List<Turma> turmas = new ArrayList<Turma>();
+    private static Usuario usuarioAtual;
 
     public Gerenciador() {
-        this.professores = new ArrayList<Professor>();
-        this.alunos = new ArrayList<Aluno>();
-        this.monitores = new ArrayList<Monitor>();
-        this.disciplinas = new ArrayList<Disciplina>();
-        this.turmas = new ArrayList<Turma>();
+        super();
     }
 
-    public boolean login(String email, String senha){
+    public static boolean login(String email, String senha){
         //verifica nas listas se existe algum usuário com o email e senha
 
         //se encontrar, altera o atributo usuarioAtual e retorna true
@@ -27,15 +26,40 @@ public class Gerenciador {
         return true;
     }
 
-    public Usuario deslogar(){
-        Usuario usuarioLogado = this.usuarioAtual;
-        this.usuarioAtual = null;
+    public static Usuario deslogar(){
+        Usuario usuarioLogado = usuarioAtual;
+        usuarioAtual = null;
         return usuarioLogado;
     }
 
-    public void adicionarAluno(Aluno aluno){
-        this.alunos
+    public static void adicionarAluno(Aluno aluno){
+        if(aluno == null) throw new NullPointerException("[Adicionar Aluno] O aluno a ser adicionado não pode ser nulo");
+        alunos.add(aluno);
     }
+
+    public static Aluno removerAluno(int ra){
+        Aluno alunoEncontrado = buscaAluno(ra);
+        alunos.remove(alunoEncontrado);
+        return alunoEncontrado;
+    }
+
+    public static List<Aluno> buscarTodosAlunos(){
+        return alunos;
+    }
+
+    public static Aluno buscaAluno(int ra){
+        Aluno alunoEncontrado = alunos.stream().filter(aluno -> {
+            return aluno.ra == ra;
+        }).collect(Collectors.toList()).get(0);
+
+        return alunoEncontrado;
+    }
+
+    public static void adicionarProfessor(Professor professor){
+       // return alunos;
+    }
+
+
 
     public List<Professor> getProfessores() {
         return professores;
