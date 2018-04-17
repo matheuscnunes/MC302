99df875d1e60479651488b83e6ee7c484e12727c
gibatalha <giovanna.batalha03@gmail.com>
2018-04-17 15:03:53 -0300

    Finalizada implementação dos métodos. Com erro, porque não tem as classes Prof, Aluno etc

--- src/main/java/Gerenciador.java
+++ src/main/java/Gerenciador.java
@@ -17,8 +17,10 @@ public class Gerenciador {
         super();
     }
 
+    ////////////         Métodos de gerenciamento de Login  ///////////////////////
+
     public static boolean login(String email, String senha){
-        //verifica nas listas se existe algum usuário com o email e senha
+        //verifica nas listas se existe algum usuário com o email e senha recebidos no método
 
         //se encontrar, altera o atributo usuarioAtual e retorna true
 
@@ -32,13 +34,27 @@ public class Gerenciador {
         return usuarioLogado;
     }
 
+    ////////////         Métodos de gerenciamento de Alunos  ///////////////////////
     public static void adicionarAluno(Aluno aluno){
         if(aluno == null) throw new NullPointerException("[Adicionar Aluno] O aluno a ser adicionado não pode ser nulo");
+
+        Aluno alunoEncontrado = buscaAluno(aluno.getRA());
+
+        if(alunoEncontrado != null){
+            throw new Error("[Adicionar Aluno] Aluno com RA " + aluno.getRA() + " já existe.");
+        }
+
         alunos.add(aluno);
     }
 
     public static Aluno removerAluno(int ra){
         Aluno alunoEncontrado = buscaAluno(ra);
+
+        if(alunoEncontrado == null){
+            //Aluno não existe na base;
+            return null;
+        }
+
         alunos.remove(alunoEncontrado);
         return alunoEncontrado;
     }
@@ -48,17 +64,164 @@ public class Gerenciador {
     }
 
     public static Aluno buscaAluno(int ra){
-        Aluno alunoEncontrado = alunos.stream().filter(aluno -> {
-            return aluno.ra == ra;
-        }).collect(Collectors.toList()).get(0);
+        List<Aluno> alunosEncontrados = alunos.stream().filter(aluno -> {
+            return aluno.getRA() == ra;
+        }).collect(Collectors.toList());
 
-        return alunoEncontrado;
+        if(alunosEncontrados.size() >= 1){
+            return alunosEncontrados.get(0);
+        }
+
+        return null;
     }
 
+    ////////////         Métodos de gerenciamento de Professores  ///////////////////////
     public static void adicionarProfessor(Professor professor){
-       // return alunos;
+        if(professor == null) throw new NullPointerException("[Adicionar Professor] O professor a ser adicionado não pode ser nulo");
+
+        //Validar se professor já existe na lista de professores antes de inserir
+
+        professores.add(professor);
     }
 
+    public static Professor removerProfessor(int id){
+        Professor profEncontrado = buscaProfessor(id);
+
+        if(profEncontrado == null){
+            //Professor não existe na base;
+            return null;
+        }
+
+        professores.remove(profEncontrado);
+        return profEncontrado;
+    }
+
+    public static List<Professor> buscarTodosProfessores(){
+        return professores;
+    }
+
+    public static Professor buscaProfessor(int id){
+        List<Professor> professoresAchados = professores.stream().filter(professor -> {
+            return professor.getID() == id;
+        }).collect(Collectors.toList());
+
+        if(professoresAchados.size() >= 1){
+            return professoresAchados.get(0);
+        }
+
+        return null;
+    }
+
+    ////////////         Métodos de gerenciamento de Monitores  ///////////////////////
+    public static void adicionarMonitor(Monitor monitor){
+        if(monitor == null) throw new NullPointerException("[Adicionar Monitor] O monitor a ser adicionado não pode ser nulo");
+
+        //Validar se monitor já existe na lista de monitores antes de inserir
+
+        monitores.add(monitor);
+    }
+
+    public static Monitor removerMonitor(int ra){
+        Monitor monitorEncontrado = buscaMonitor(ra);
+
+        if(monitorEncontrado == null){
+            //Monitor não existe na base;
+            return null;
+        }
+
+        monitores.remove(monitorEncontrado);
+        return monitorEncontrado;
+    }
+
+    public static List<Monitor> buscarTodosMonitores(){
+        return monitores;
+    }
+
+    public static Monitor buscaMonitor(int ra){
+        List<Monitor> monitorersAchados = monitores.stream().filter(monitor -> {
+            return monitor.getRA() == ra;
+        }).collect(Collectors.toList());
+
+        if(monitorersAchados.size() >= 1){
+            return monitorersAchados.get(0);
+        }
+
+        return null;
+    }
+
+    ////////////         Métodos de gerenciamento de Disciplinas  ///////////////////////
+    public static void adicionarDisciplina(Disciplina disciplina){
+        if(disciplina == null) throw new NullPointerException("[Adicionar Disciplina] A disciplina a ser adicionado não pode ser nula");
+
+        //Validar se a disciplina já existe na lista de disciplinas antes de inserir
+
+        disciplinas.add(disciplina);
+    }
+
+    public static Disciplina removerDisciplina(String codigo){
+        Disciplina disciplinaEncontrada = buscaDisciplina(codigo);
+
+        if(disciplinaEncontrada == null){
+            //Disciplina não existe na base;
+            return null;
+        }
+
+        disciplinas.remove(disciplinaEncontrada);
+        return disciplinaEncontrada;
+    }
+
+    public static List<Disciplina> buscarTodasDisciplinas(){
+        return disciplinas;
+    }
+
+    public static Disciplina buscaDisciplina(String codigo){
+        List<Disciplina> disciplinasAchadas = disciplinas.stream().filter(disciplina -> {
+            return disciplina.getCodigo().equals(codigo);
+        }).collect(Collectors.toList());
+
+        if(disciplinasAchadas.size() >= 1){
+            return disciplinasAchadas.get(0);
+        }
+
+        return null;
+    }
+
+    ////////////         Métodos de gerenciamento de Turmas  ///////////////////////
+    public static void adicionarTurma(Turma turma){
+        if(turma == null) throw new NullPointerException("[Adicionar Turma] A turma a ser adicionado não pode ser nula");
+
+        //Validar se a turma já existe na lista de turmas antes de inserir
+
+        turmas.add(turma);
+    }
+
+    public static Turma removerTurma(int id){
+        Turma turmaEncontrada = buscaTurma(id);
+
+        if(turmaEncontrada == null){
+            //Turma não existe na base;
+            return null;
+        }
+
+        turmas.remove(turmaEncontrada);
+        return turmaEncontrada;
+    }
+
+    public static List<Turma> buscarTodasTurmas(){
+        return turmas;
+    }
+
+    public static Turma buscaTurma(int id){
+        List<Turma> turmasEncontradas = turmas.stream().filter(turma -> {
+            return turma.getId() == id;
+        }).collect(Collectors.toList());
+
+        if(turmasEncontradas.size() >= 1){
+            return turmasEncontradas.get(0);
+        }
+
+        return null;
+    }
 
 
     public List<Professor> getProfessores() {
